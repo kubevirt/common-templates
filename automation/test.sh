9172106da4bcaa2b0b88352e06ab78b96c6be3a3
Karel Simon <ksimon@redhat.com>
2020-09-18 08:34:18 +0200

    fix virtctl link
    Signed-off-by: Karel Simon <ksimon@redhat.com>

--- automation/test.sh
+++ automation/test.sh
@@ -42,9 +42,9 @@ make generate
 for filename in dist/templates/*; do
     sed -i -e 's/^\(\s*terminationGracePeriodSeconds\s*:\s*\).*/\10/' $filename
 done
-  
+
 curl -Lo virtctl \
-    https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_VERSION/virtctl-$KUBEVIRT_VERSION-linux-amd64
+    https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_VERSION/virtctl-$KUBEVIRT_VERSION-linux-x86_64
 chmod +x virtctl
 
 
