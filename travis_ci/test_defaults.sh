28008a4c7b8998ed05fe991d9c0fb980b0bf3e2b
Jed Lejosne <jed@redhat.com>
2020-10-14 12:55:01 -0400

    travis-ci: ensure exactly 1 default variant is set per OS
    Signed-off-by: Jed Lejosne <jed@redhat.com>

---
+++ travis_ci/test_defaults.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# This file is part of the KubeVirt project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright 2020 Red Hat, Inc.
+#
+set -ex
+
+templates_dir='dist/templates'
+default_label='template.kubevirt.io/default-os-variant=true'
+os_label='os.template.kubevirt.io/'
+template='{{range .items}}{{range $key, $val := .metadata.labels}}{{if eq $val "true"}}{{$key}}{{"\n"}}{{end}}{{end}}{{end}}'
+
+make generate
+oc create -f $templates_dir
+
+# Ensure exactly one default variant per OS
+for os in $(oc get templates -o go-template="$template" | grep "^$os_label" | sort -uV); do
+    defaults=$(oc get template -l ${os}=true,template.kubevirt.io/default-os-variant=true -o name | wc -l)
+    if [[ $defaults -eq 1 ]]; then
+	continue
+    elif [[ $defaults -eq 0 ]]; then
+	echo "Error: No default variant set for $os"
+	exit 1
+    else
+	echo "Error: There can only be 1 default variant per OS. $os has $defaults"
+	exit 1
+    fi
+done
