4355f1dae79e1c337720520a07bd9597b93518da
Shweta Padubidri <spadubid@redhat.com>
2021-04-10 18:08:46 -0400

    Use the Fedora Release url - https://getfedora.org/releases.json to identify the latest releases, and their image download links.
    Signed-off-by: Shweta Padubidri <spadubid@redhat.com>
    
    Support the Centos periodic job to create a new container-disk-image
    when a new release is available
    
    Signed-off-by: Shweta Padubidri <spadubid@redhat.com>
    
    Revert "Support the Centos periodic job to create a new container-disk-image"
    
    This reverts commit f52c2ff7e3ff59132b2878d2501062d62dd6152f.

--- dvtemplates/fedora_new_image.sh
+++ dvtemplates/fedora_new_image.sh
@@ -2,7 +2,7 @@
 
 error_message() {
     if [ $1 -eq 1 ]; then
-        echo -e "Unable to connect to the Fedora repository"
+        echo -e "Unable to access data from the Fedora release URL"
     elif [ $1 -eq 2 ]; then
         echo -e "Invalid Image Version"
     elif [ $1 -eq 3 ]; then
@@ -26,11 +26,10 @@ port=5000
 
 #set the final registry to hold the fedora images
 FEDORA_REPO="quay.io/kubevirt/fedora-images"
-BASE_URL=https://download.fedoraproject.org/pub/fedora/linux/releases/
+RELEASE_URL=https://getfedora.org/releases.json
 
-wget -qO index.html $BASE_URL || exit 1
-FEDORA_VERSION=`cat index.html | sed -e 's/.*>\(.*\)\/<.*/\1/' | sort -rn | head -n 1 | tr -d ' '`
-re='^ *[0-9]+ *$'
+FEDORA_VERSION=`curl $RELEASE_URL | jq '.[] | select(.version|test("^[0-9]+$")) | .version' | sort -rn | uniq | head -n 1 | tr -d '"'` || exit 1
+re='^[0-9]+$'
 if ! [[ $FEDORA_VERSION =~ $re ]] ; then
     error_message 2
 fi
@@ -39,24 +38,18 @@ echo "Latest Fedora version is : ${FEDORA_VERSION}"
 # Fetch the old image
 docker pull -a $FEDORA_REPO
 
-image_tag=$(docker images $FEDORA_REPO --format "{{json .Tag }}" | sort -rn | head -n 1)
-FEDORA_OLD_VERSION=`echo "$image_tag" | tr -d '"'`
-echo "Fedora version in the Image Registry is : ${FEDORA_OLD_VERSION}"
+FEDORA_OLD_VERSION=$(docker images $FEDORA_REPO --format "{{json .}}" | jq 'select(.Tag|test("^[0-9]+$")) | .Tag' | sort -rn | head -n 1 | tr -d '"')
 
-if [ -z "$image_tag" ]; then
+echo "Fedora version in the Image Registry is : ${FEDORA_OLD_VERSION}"
+if [ -z "${FEDORA_OLD_VERSION}" ]; then
     echo -e "No Container Image found in the registry"
 fi
-
 
 if [[ "$FEDORA_VERSION" == "$FEDORA_OLD_VERSION" ]]; then
     error_message 4
 fi
 
-IMAGE_URL=${BASE_URL}${FEDORA_VERSION}/Cloud/x86_64/images/
-wget -qO image.html $IMAGE_URL || exit 3
-FEDORA_IMAGE=$(grep Fedora.*qcow2 image.html | sed -e 's/.*>\(Fedora.*qcow2\)<.*/\1/')
-
-URL=${IMAGE_URL}${FEDORA_IMAGE}
+URL=`curl $RELEASE_URL | jq '.[] | select(.link|test(".*qcow2")) | select(.version=='\"$FEDORA_VERSION\"' and .variant=="Cloud" and .arch=="x86_64") | .link' | tr -d '"'` || exit 1
 
 wget -q --show-progress $URL || exit 3
 echo -e "Successfully downloaded new Fedora $FEDORA_VERSION image"
