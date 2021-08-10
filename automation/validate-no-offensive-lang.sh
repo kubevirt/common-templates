157425b0c09af4bef85d4fab07db9111228fa487
Vatsal Parekh <vparekh@redhat.com>
2021-08-10 21:14:21 +0530

    Exclude vendor from offensive-language check
    Signed-off-by: Vatsal Parekh <vparekh@redhat.com>

--- automation/validate-no-offensive-lang.sh
+++ automation/validate-no-offensive-lang.sh
@@ -3,7 +3,7 @@
 OFFENSIVE_WORDS="black[ -]?list|white[ -]?list|master|slave"
 ALLOW_LIST=".+[:/=]master[a-zA-Z]*/?"
 
-if git grep -inE "${OFFENSIVE_WORDS}" -- ":!${BASH_SOURCE[0]}" ':!.github/workflows/' | grep -viE "${ALLOW_LIST}"; then
+if git grep -inE "${OFFENSIVE_WORDS}" -- ":!${BASH_SOURCE[0]}" ':!.github/workflows/' ':!./vendor/' | grep -viE "${ALLOW_LIST}"; then
   echo "Validation failed. Found offensive language"
   exit 1
 fi
