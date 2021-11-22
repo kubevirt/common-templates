9adf092f6e77eb957035dd2fbb2bcba886a0612b
Roman Mohr <rmohr@redhat.com>
2021-11-22 13:03:35 +0100

    Add a RHEL8 template targeting SAP HANA as workload
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- automation/unit-tests.sh
+++ automation/unit-tests.sh
@@ -3,9 +3,10 @@ set -ex
 make generate
 
 #syntax check
-templates=`ls dist/templates/*`
+templates=$(ls dist/templates/*)
 namespace="kubevirt"
 
+echo "Testing fresh installation..."
 oc apply -f - <<EOF
 apiVersion: v1
 kind: Namespace
@@ -17,27 +18,30 @@ oc project $namespace
 
 echo "Printing templates to stdout for debugging"
 for template in $templates; do
-    cat "$template"
+  cat "$template"
 done
 
 echo "Processing all templates to find syntax issues"
 for template in $templates; do
-    oc process -f "$template" NAME=test SRC_PVC_NAME=test || exit 1;
+  if [[ $template =~ .*saphana.* ]]; then
+    oc process -f "$template" NAME=test TARGET_NODE_NAME=mynode || exit 1
+  else
+    oc process -f "$template" NAME=test SRC_PVC_NAME=test || exit 1
+  fi
 done
 
-echo "Testing fresh installation..."
 oc create -f dist/templates
 
 #check validation part
 python3 automation/check-validations.py
-if [ $? -eq 1 ];then
+if [ $? -eq 1 ]; then
   echo "Validation of validations failed"
   exit 1
 fi
 
-#check minimal memory again 
+#check minimal memory again
 python3 automation/validate-min-memory-consistency.py
-if [ $? -eq 1 ];then
+if [ $? -eq 1 ]; then
   echo "Validation of memory requirements failed"
   exit 1
 fi
@@ -45,8 +49,8 @@ fi
 echo "Testing upgrade..."
 oc delete -f dist/templates
 
-LATEST_CT=$(curl -L https://api.github.com/repos/kubevirt/common-templates/releases | \
-            jq '.[] | select(.prerelease==false) | .name' | sort -V | tail -n1 | tr -d '"')
+LATEST_CT=$(curl -L https://api.github.com/repos/kubevirt/common-templates/releases |
+  jq '.[] | select(.prerelease==false) | .name' | sort -V | tail -n1 | tr -d '"')
 oc apply -f https://github.com/kubevirt/common-templates/releases/download/${LATEST_CT}/common-templates.yaml
 
 set +e
@@ -54,7 +58,7 @@ python3 automation/validate-pvc-name-stability.py
 RC=${?}
 set -e
 echo "[Upgrade][test_id:5749] Validation of PVC name stability"
-if [ ${RC} -ne 0 ];then
+if [ ${RC} -ne 0 ]; then
   echo "[Upgrade][test_id:5749] Validation of PVC name stability failed"
   exit ${RC}
 fi
@@ -62,13 +66,13 @@ fi
 oc apply -f dist/templates
 
 ./automation/test_duplicate_templates.sh
-if [ $? -eq 1 ];then
+if [ $? -eq 1 ]; then
   echo "[Upgrade] Validation of duplicate templates failed "
   exit 1
 fi
 
-./automation/test_defaults.sh 
-if [ $? -eq 1 ];then
+./automation/test_defaults.sh
+if [ $? -eq 1 ]; then
   echo "[Upgrade] Validation of default label failed "
   exit 1
 fi
