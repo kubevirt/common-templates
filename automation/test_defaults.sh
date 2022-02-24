cb894147579a32a674adc12650cdabaaa1179d27
Felix Matouschek <fmatouschek@redhat.com>
2022-02-24 12:03:24 +0100

    Improve existing unit tests
    Improve the existing unit tests in automation/test_defaults.sh and
    automation/test_duplicate_templates.sh by using yq instead of grepping
    yaml files.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- automation/test_defaults.sh
+++ automation/test_defaults.sh
@@ -18,25 +18,28 @@
 #
 set -ex
 
-templates_dir='dist/templates'
-default_label='template.kubevirt.io/default-os-variant=true'
-os_label='os.template.kubevirt.io/'
-template='{{range .items}}{{range $key, $val := .metadata.labels}}{{if eq $val "true"}}{{$key}}{{"\n"}}{{end}}{{end}}{{end}}'
-ver_label='^[[:space:]]*template.kubevirt.io/version'
-# Any template can be used here as they all have the same verison label, centos is an arbitrary one
-ver_value=$(grep "$ver_label" $templates_dir/centos6-server-large.yaml | tail -1 | cut -f2 -d":" | tr -d ' "')
-ver_label="template.kubevirt.io/version=$ver_value"
+# Any template can be used here as they all have the same version label, fedora is an arbitrary one
+ver_label=$( \
+  yq -o props '.metadata.labels | with_entries(select(.key == "template.kubevirt.io/version"))' \
+  dist/templates/fedora-server-large.yaml \
+)
+
+readarray -t oss < <( \
+  oc get templates -o yaml | \
+  yq -o props '.items[].metadata.labels | . as $item ireduce ({}; . * $item) | with_entries(select(.key == "os.template.kubevirt.io/*"))' \
+)
 
 # Ensure exactly one default variant per OS
-for os in $(oc get templates -o go-template="$template" | grep "^$os_label" | sort -uV); do
-    defaults=$(oc get template -l ${os}=true,template.kubevirt.io/default-os-variant=true,$ver_label -o name | wc -l)
-    if [[ $defaults -eq 1 ]]; then
-	    continue
-    elif [[ $defaults -eq 0 ]]; then
-        echo "Error: No default variant set for $os"
-        exit 1
-    else
-        echo "Error: There can only be 1 default variant per OS. $os has $defaults"
-        exit 1
-    fi
+for os in "${oss[@]}"; do
+  defaults=$(oc get template -l "$os,template.kubevirt.io/default-os-variant = true,$ver_label" -o name | wc -l)
+
+  if [[ $defaults -eq 1 ]]; then
+    continue
+  elif [[ $defaults -eq 0 ]]; then
+    echo "Error: No default variant set for $os"
+    exit 1
+  else
+    echo "Error: There can only be 1 default variant per OS. $os has $defaults"
+    exit 1
+  fi
 done
